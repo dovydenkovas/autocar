d5f35921eb17eb0e7c6a78bc7395b6625e6be4fe
dovydenkovas <dovydenkov2003@mail.ru>
2021-10-16 07:37:55 +0300

    Add License

--- src/arduinoemulator/arduinoemulator.py
+++ src/arduinoemulator/arduinoemulator.py
@@ -1,4 +1,26 @@
+"""
+    This file is part of https://github.com/dovydenkovas/autocar project.
+
+    Copyright 2021 The https://github.com/dovydenkovas/autocar contributors
+
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
 import os, subprocess, time, serial
+
 
 # this script lets you emulate a serial device
 # the client program should use the serial port file specifed by client_port
